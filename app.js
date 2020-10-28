af38952c64c5604318f0cc021a802a845cc6278c
jtreeves <jackson.t.reeves@gmail.com>
2020-10-28 17:29:53 -0400

    [interaction] Allow user to input a request and generate results based on their query

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-document.getElementById('submit-button')
+document.getElementById('submit-button').addEventListener('click', function() {
+    const genre = document.getElementById('genre-input').value;
+    fetchBook(genre);
+});
 
 function fetchBook(genre) {
     fetch(`http://openlibrary.org/subjects/${genre}.json`)
@@ -10,7 +13,7 @@ function fetchBook(genre) {
         const cover = `http://covers.openlibrary.org/b/id/${randomBook.cover_id}-M.jpg`;
         appendBookToDom(title, author, cover);
     })
-}
+};
 
 function appendBookToDom(title, author, cover) {
     const titleH3 = document.createElement('h3');
@@ -20,17 +23,14 @@ function appendBookToDom(title, author, cover) {
     const coverImage = document.createElement('img');
     coverImage.setAttribute('src', `${cover}`);
     document.querySelector('#display-book').append(titleH3, authorH5, coverImage);
-}
+};
 
 function getRandomBook(books) {
     const randomIndex = Math.floor(Math.random()*books.length);
     return books[randomIndex];
-}
+};
 
 // Temporary; needs to be improved around to handle multiple authors
 function getAuthor(book) {
     return book.authors[0].name;
-}
-
-let genre = 'mystery';
-fetchBook(genre);
+};
